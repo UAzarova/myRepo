bc59932fcb19b76c0dad76c8898cb7a7749a72ee
UAzarova <abstract.time@gmail.com>
2022-05-12 16:11:03 +0300

    hw2

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+/**
+ * Java 3. Home work 2.
+ *
+ * @author Azarova Iuliia
+ * @version 12.05.2022
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("My empty project");
+    }
+}
